baf6df5d4852e62050479663d3bff392a7a91d6f
Pranjal Agrawal <46265193+Pranjal-R-Agrawal@users.noreply.github.com>
2023-07-18 19:47:08 +0530

    Added macros to toggle and read walls

--- Micromouse/Micromouse.ino
+++ Micromouse/Micromouse.ino
@@ -1,15 +1,39 @@
+/** Resources
+  * https://docs.arduino.cc/learn/programming/bit-math
+  * http://www.123seminarsonly.com/Seminar-Reports/038/59360985-Maze-Solving-Algorithms.pdf
+  * https://github.com/Craga89/Micromouse/
+*/
+
 #define rows 16
 #define cols 16
+
 #define linearse(row, col) row * cols + col
 #define delinearise_row(location) location / cols
 #define delinearise_col(location) location % cols
 
-byte distanceArray[rows * cols];
+#define toggleWall(location, direction) floodArray[location].neighbours |= 1 << direction
+#define readWall(location, direction) bitRead(floodArray[location].neighbours, direction)
+
+#define North 0
+#define East 1
+#define South 2
+#define West 3
+
+struct cell{
+  byte flood;
+  byte neighbours;
+};
+
+cell floodArray[rows * cols];
 
 void setup() {
-
+  Serial.begin(9600);
+  for(byte i = 0; i < 256; i++) {
+    floodArray[i].flood = 255;
+    floodArray[i].neighbours = 0;
+    if (i == 255) break;
+  }
 }
 
 void loop() {
-
 }
